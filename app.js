1ba517a7ef9396ccf327a5a14d4b1b2a80fb9d2d
Jyothi Lakshmi <jyothilakshmi5221@gmail.com>
2022-03-21 12:31:16 +0530

    NEWgit

--- app.js
+++ app.js
@@ -71,6 +71,10 @@
 //     })
 // })
 
- app.listen(5000);
-  console.log("Server is ready at port no 5000");
+//  app.listen(5000);
+//   console.log("Server is ready at port no 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`LibraryApp is running on the ${PORT}`);
+})
 
