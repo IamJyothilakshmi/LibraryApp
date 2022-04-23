4812b81021998cd725ef8b2391b9e0088c9135c8
Jyothi Lakshmi <jyothilakshmi5221@gmail.com>
2022-04-23 19:35:10 +0530

    newgit

--- src/routes/addAuthorRouter.js
+++ src/routes/addAuthorRouter.js
@@ -2,17 +2,17 @@ const express = require('express');
 const addAuthorRouter=express.Router();
 const Authorsdata = require('../model/Authorsdata');
 function router(nav){
-    addAuthorRouter.get('/',function(_req,res){
+    addAuthorRouter.get('/',function(req,res){
         res.render('addAuthor',{
     nav,
-    title:'Add Author'
+    title:'Library'
         });
     });
     addAuthorRouter.post('/add',function(req,res){
         var items = {
             title:req.body.title,
-            // author:req.body.author,
-            // genre:req.body.genre,
+             author:req.body.author,
+             genre:req.body.genre,
             image:req.body.image,
             about:req.body.about
             
