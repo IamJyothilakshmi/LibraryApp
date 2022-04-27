fa22aa8adabc2b47a527662a565201207f7358d2
Jyothi Lakshmi <jyothilakshmi5221@gmail.com>
2022-04-27 13:45:33 +0530

    NEWgit

--- src/model/Authorsdata.js
+++ src/model/Authorsdata.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/library");
+// mongoose.connect("mongodb://localhost:27017/library");
+
+  mongoose.connect("mongodb+srv://userone:userone@ictakfiles.aladj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const Schema=mongoose.Schema;
 const AuthorSchema= new Schema({
     title:String,
--- src/model/Bookdata.js
+++ src/model/Bookdata.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/library');
+// mongoose.connect('mongodb://localhost:27017/library');
+
+ mongoose.connect("mongodb+srv://userone:userone@ictakfiles.aladj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const Schema = mongoose.Schema;
 const BookSchema = new Schema({
     title : String,
